465cd0835dc94d75fe7e72e591014ca2c08f33cf
alanupermana <alanupermana@gmail.com>
2019-04-22 03:57:27 +0700

    Initial commit

---
+++ app/src/main/java/motion/diaryapps/list_notes/ListNotesModel.java
@@ -0,0 +1,65 @@
+package motion.diaryapps.list_notes;
+
+import java.io.Serializable;
+
+/**
+ * Class ini digunakan untuk sebagai Model data pada layout item
+ */
+public class ListNotesModel {
+
+    private String id;
+    private String image_url;
+    // TODO: 4/12/19 -> Buat Attribute title dan date disini
+    // hint: untuk date gunakan tipe data string, lakukan seperti contoh diatas
+    private  String date;
+    private  String title;
+
+    // TODO: 4/12/19 -> Buat Constructor disini
+    // hint: Gunakan alt+insert
+
+    public ListNotesModel(String id, String image_url, String date, String title) {
+        this.id = id;
+        this.image_url = image_url;
+        this.date = date;
+        this.title = title;
+    }
+
+
+    // TODO: 4/12/19 -> Buat Getter dan Setternya disini
+    // hint: Gunakan alt+insert
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+
+}
